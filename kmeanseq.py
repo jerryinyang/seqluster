5999b6d1568430e179ef4546f2abcdb8e9ee3397
Jerry Inyang <jerprog0@gmail.com>
2024-03-15 23:06:28 +0100

    completed the first step of sequential learning; storing new data in batches

--- kmeanseq.py
+++ kmeanseq.py
@@ -2,13 +2,12 @@ import numpy as np
 from kmeans import KMeansPlus
 from dtaidistance import dtw
 
-
 class KMeansSeq(KMeansPlus): 
     """
     The model uses a kmeans++ clustering model, and sequentially updates the centroid definitions.
     """ 
 
-    def __init___(self, k, confluence_metric=None, learning_rate=None, batch_size=None) -> None:
+    def __init__(self, k, confluence_metric=None, learning_rate=None, batch_size=None) -> None:
         super().__init__(k)
 
         self.k = k
@@ -21,15 +20,27 @@ class KMeansSeq(KMeansPlus):
         # Store the data for the current batch
         self._data = None
 
-        self._current_batch_counts = None
-        self._current_batch_sums = None
+        # Store the base metrics
+        self._base_mean_distance = None
+        self._base_cluster_counts = None
+
+        self._batch_distance_sums = None
+        self._batch_point_counts = None
 
 
     def fit(self, X, max_iterations=200):
-        _ = super().fit(X, max_iterations)
+        labels = super().fit(X, max_iterations)
 
         # Get the centroids 
         self.centroids = kmeans.centroids
+
+        # Store the base stats
+        # Calculate distances of each point to its assigned centroid
+        distances = np.linalg.norm(X - self.centroids[labels], axis=1)
+
+        # Calculate the average intra-cluster distances per cluster
+        self._base_mean_distance = np.array([np.mean(distances[labels == label]) for label in range(self.k)])
+        self._base_cluster_counts = np.bincount(labels)
 
 
     def predict(self, X, seq_learn=False):
@@ -45,29 +56,63 @@ class KMeansSeq(KMeansPlus):
 
         # Enable sequential learning from new data
         if seq_learn:
-            pass
+            # 1:  Collect data in batches
+            # Initialize the array with the same shape as a centroid
+            if self._batch_distance_sums is None:
+                self._batch_distance_sums = np.zeros(self.k)
+                self._batch_point_counts = np.zeros(self.k)
+
+            # For each new data point, add them to the current batch sum for its respective cluster
+            labels = np.array(labels)
+            distances = np.linalg.norm(X - self.centroids[labels], axis=1)
+            sums = np.array([np.sum(distances[labels == label]) for label in range(self.k)])
+            
+            print(sums)
+            exit()
+            # for label in np.unique(labels):
+
+            #     # Select only data points of that label
+            #     mask = labels == label
+            #     points = X[mask]
+
+            #     summed = np.sum(points, axis=0)
+            #     summed = summed.T.reshape(-1)
+
+            #     # Update the sums
+            #     self._batch_sums[label] += summed
+            #     self._batch_counts[label] += len(points)
+
+            #     self.evaluate_centroids(label)
 
         return labels
+    
+
+    def update_centroid(self, centroid, sum, count):
+        """
+        Computes the adjusted centroid values, based on recent batch data
+        """
+        return ((1 - self.learning_rate) * centroid) + (self.learning_rate * (sum/ count))
 
 
-    def update_centroid(self):
-        return
-
-
-    def evaluate_centroids(self):
+    def evaluate_centroids(self, label):
         """
         Evaluate the centroids with the current batch's data
         """
 
-        # For each centroid
-        for cluster in range(self.k):
-            # Compute the new centroid
-            new_data_sum = self._current_batch_sums
+        # Compute the new centroid
+        centroid = self.centroids[label]
+        sum = self._batch_sums[label]
+        count = self._batch_counts[label]
+        
+        # Evaluate the difference in the confluence metric, if it exceeds the threshold
+        new_centroid = self.update_centroid(centroid, sum, count)
 
-            # Evaluate the difference in the confluence metric, if it exceeds the threshold
+        # For threshold centroid difference, compare current centroid with the calculated centroid
+        pass
         
-        
+    
         return
+
 
     def _compute_confluence_metric(self):
         """
@@ -82,24 +127,25 @@ class KMeansSeq(KMeansPlus):
 
         return
     
-
         
-        
-        
-        
-    
 if __name__ == "__main__":
     import pandas as pd
     import matplotlib.pyplot as plt  # noqa
 
+    np.random.seed(14)
+
     data = pd.read_csv("/Users/jerryinyang/Code/seqluster/data.csv")
 
     X = data[['x', 'y']].to_numpy()
+    X = np.random.permutation(X)
 
     kmeans = KMeansSeq(4)
     
     labels = kmeans.fit(X)
-    print(kmeans.centroids)
+
+    kmeans.predict(X[-50:], seq_learn=True)
+    # print(kmeans._current_batch_sums)
+    # print(kmeans._current_batch_counts)
 
     # plt.scatter(X[:,0], X[:,1], c=labels)
     # plt.scatter(kmeans.centroids[:, 0], kmeans.centroids[:, 1], c='fuchsia', marker='*', s=200)
