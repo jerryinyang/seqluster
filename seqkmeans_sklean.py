686f44ccd57a561bbbbfb070ebe9b9f1a829afd1
Jerry Inyang <jerprog0@gmail.com>
2024-03-29 22:26:50 +0100

    completed the implementation of the seqlearn

--- seqkmeans_sklean.py
+++ seqkmeans_sklean.py
@@ -10,12 +10,11 @@ class SeqKMeans:
     The model uses a kmeans++ clustering model, and sequentially updates the centroid definitions.
     """ 
 
-    def __init__(self, k, confluence_metric=None, batch_size=100, learning_rate=.001, centroid_update_threshold_std=5, random_state=14, verbose=False) -> None:
+    def __init__(self, k, confluence_metric=None, distance_metric='euclidean', learning_rate=.001, centroid_update_threshold_std=5, random_state=14, verbose=False) -> None:
         # Stores Cluster Centroids
         self.k = k
         self.confluence_metric = confluence_metric
         self.learning_rate = learning_rate
-        self.batch_size = batch_size
 
         # Store KMeans variables
         self.kmeans = None
@@ -101,7 +100,7 @@ class SeqKMeans:
 
         # 2. Compute the new data's average intra-cluster distance
         labels_count = np.bincount(labels, minlength=self.k)
-        centroid_point_distances = np.linalg.norm(X - self.centroids[labels], axis=1)
+        centroid_point_distances = self._compute_distances(X, labels)
         intracluster_distance_sums = np.array([np.sum(centroid_point_distances[labels == label], axis=0) for label in cluster_ids])
 
         # 3. Compute the centroid data point for each cluster
@@ -223,6 +222,18 @@ class SeqKMeans:
         return
    
 
+    def _compute_distances(self, X, labels):
+
+        distances = []
+        
+        for index in range(len(X)):
+            label = labels[index]
+            distances.append((dtw.distance_fast(X[index], self.centroids[label])))
+
+        return np.array(distances)
+        # return np.linalg.norm(X - self.centroids[labels], axis=1)
+
+
     def _verbose_output(self, *args):
         if self._verbose:
             for content in args:
@@ -257,7 +268,7 @@ if __name__ == "__main__":
     kmeans = SeqKMeans(4, learning_rate=0.00000001, verbose=False)
     labels = kmeans.fit(X)
 
-    for iter in range(20):
+    for iter in range(1000):
         random_indices = np.random.randint(0, len(X), size=400)
         test_data = X[random_indices]
 
@@ -270,53 +281,4 @@ if __name__ == "__main__":
     # plt.scatter(X[:,0], X[:,1], c=labels)
     # plt.scatter(kmeans.centroids[:, 0], kmeans.centroids[:, 1], c='fuchsia', marker='*', s=200)
     # plt.show()
-        
-
-
-
-# if __name__ == "__main__":
-
-    # import pandas as pd
-    # import matplotlib.pyplot as plt  # noqa
-    
-    # np.random.seed(14)
-
-    # # region 1. Prepare the data s
-    # prices = pd.read_parquet('prices.parquet')
-
-    # raw_data = prices["close"].dropna(axis=0)
-    # raw_data = raw_data.to_numpy()
-
-    # # Generate windows
-    # window_size = 5
-
-    # X = []
-    
-    # for index in range(window_size, len(raw_data)):
-    #     start_index = index - window_size
-    #     end_index = index
-
-    #     X.append(raw_data[start_index : end_index])
-
-    # # Split X Data
-    # split_percent = .7
-    # split_index = int(round(len(X) * split_percent))
-    
-    # X = np.random.permutation(X)
-    # X, X_test = np.array(X[:split_index]), np.array(X[split_index:])
-    # X_test = np.random.permutation(X_test)
-
-    # # endregion
-
-    # kmeans = SeqKMeans(8)
-    # labels = kmeans.fit(X)
-
-    # print(kmeans.predict(X_test, seq_learn=True))
-
-    # # cluster1 = X[labels == 1]
-
-    # # print(cluster1.shape)
-
-    # # plt.plot(cluster1[:10])
-    # # plt.show()
         
